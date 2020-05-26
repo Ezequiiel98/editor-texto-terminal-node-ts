7ee938c78831e8d2920ffc8671fabd7d53a7dfab
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-05-25 23:52:03 -0300

    termino la clase document.ts

---
+++ app/document.ts
@@ -0,0 +1,64 @@
+import * as fs from 'fs';
+import * as os from 'os';
+
+class Document{
+  private _content: string;
+  private _isSaved: boolean;
+  private _filename: string;
+  private _dir: string;
+
+  constructor(dir: string){
+    this._content = '';
+    this._isSaved = false;
+    this._filename = '';
+    this._dir = dir;
+  }
+  
+  exist(name: string): boolean{
+    return fs.existsSync(`${this._dir}/${name}`);
+  }
+  
+  apprend(text: string): void{
+    this._content += os.EOL + text;
+    this._isSaved = false
+  }
+  
+  saveAs(name: string): void{
+    fs.writeFileSync(`${this._dir}/${name}`, this._content);
+
+    this._isSaved = true;
+    this._filename = name;
+  }
+
+  save(): void{
+    fs.writeFileSync(`${this._dir}/${this._filename}`, this._content);
+
+    this._isSaved = true;
+    this._filename = this._filename;
+  }
+
+  getContent(): string{
+    return this._content;
+  }
+
+  hasName(): boolean{
+    return this._filename !== '';
+  }
+  
+  getName(): string{
+    return this._filename;
+  }
+
+  isSaved(){
+    return this._isSaved;
+  }
+
+  open(name: string): string{
+    this._content = fs.readFileSync(`${this._dir}/${name}`, 'utf-8');
+    this._filename= name;
+    this._isSaved = true;
+    return this._content;
+  }
+}
+
+module.exports = Document;
